b4469dbf338a66963704883073eefde8a61c44a8
arunjyothishk <35922218+arunjyothishk@users.noreply.github.com>
2018-09-30 09:08:54 +0530

    Add files via upload

---
+++ _cayenne/_cayenne.ino
@@ -0,0 +1,40 @@
+#include <CayenneMQTTESP8266.h>
+#include <ESP8266WiFi.h>
+#include <Servo.h>
+#define ldr A0
+#define servo 9
+Servo myservo;
+ char ssid[]= "jyothu";
+ char pass[]= "password";
+
+  char username[]="jdfja";
+  char password[] ="jafa;";
+  char client_id[]="sajfdj";
+
+  void setup(){
+
+
+    pinMode(ldr,INPUT);
+   pinMode (servo,OUTPUT);
+   Serial.begin(9600);
+   Cayenne.begin(username,password,client_id,ssid,pass);
+   
+  }
+void loop(){
+
+
+int val=analogRead(ldr);
+int loc=map(val,0,1024,0,100);
+
+Cayenne.loop();
+
+
+Cayenne.virtualWrite(0,loc);
+
+}
+CAYENNE_IN(1){
+
+myservo.write(getValue.asInt());
+
+}
+
---
+++ ldr_data_push/ldr_data_push.ino
@@ -0,0 +1,30 @@
+#include <CayenneMQTTESP8266.h>
+#include <ESP8266WiFi.h>
+
+char ssid[] = "Red";
+char password[] = "enthalee";
+
+char username[] = "27b8ffd0-96fc-11e7-94c8-a1bba3f5296f";
+char mqtt_password[] = "8f0fe6b7bc0caae6cf8174c5032bda15e453d782";
+char client_id[] = "0441b680-f6ad-11e7-939f-b354e6d68235";
+
+int ldr = A0;
+int ldr_data;
+
+void setup()
+{
+
+    Cayenne.begin(username,mqtt_password,client_id,ssid,password);
+
+    pinMode(A0,INPUT);
+  
+}
+
+void loop()
+{
+    Cayenne.loop();
+  
+    ldr_data = analogRead(ldr);
+
+    Cayenne.virtualWrite(0, ldr_data);
+}
